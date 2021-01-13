35272e9dac264107ab110120ad9ca1e25f6d4b21
joseph san nicolas <34865831+goldenexecutive@users.noreply.github.com>
2021-01-13 10:05:06 -0800

    making progress handling the comunication

--- server.js
+++ server.js
@@ -8,16 +8,12 @@ const body_parser = require('body-parser')
 const jsonwbtoken = require('jsonwebtoken')
 const limiter     = require('express-rate-limit')
 const routes      = require('./API/routes')
+const bcrypt      = require('bcrypt')
+const credentials = require('./credentials')
 //-->initializations
 const app = express()
 
 
-var token = jsonwbtoken.sign({
-    name:"Dude",
-    data:["data1","data2","data1","data2"]
-},'secret')
-
-//-->functions
 
 app.use(body_parser.json({}))
 app.use(body_parser.urlencoded({extended:true}))
@@ -25,6 +21,17 @@ app.use(routes.Get)
 app.use(routes.Set)
 app.use(routes.Del)
 
+app.post("/:username&:password",function(req,res){
+    if (req.params.username == credentials.username && req.params.password == credentials.password){
+        var token = jsonwbtoken.sign({
+            name:"Dude",
+            data:["data1","data2","data1","data2"]
+        },'veryinterestingtext')
+        res.send(token)
+    } else {
+        res.send('sorry not logged in ')
+    }
+})
 
 app.get('/',async(req,res)=>{
     res.redirect("https://google.com")
